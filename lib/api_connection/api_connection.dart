105af57556eecaad8a783be86c0a31f30cce4d5a
KTharsiss <lianmiguel1@gmail.com>
2023-11-30 23:57:12 -0500

    Actualizada la ruta de los php para que se conecten al servidor en azure

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -1,14 +1,14 @@
 class API{
-  static const hostConnect = 'http://192.168.0.101/api/conexion.php';
-  static const consultalogin = 'http://192.168.0.101/api/consultalogin.php';
-  static const consultasalones = 'http://192.168.0.101/api/consultasalones.php';
-  static const consultadisponibilidades = 'http://192.168.0.101/api/consultadisponibilidades.php';
-  static const updateperfil = 'http://192.168.0.101/api/updateperfil.php';
-  static const consultareservas = 'http://192.168.0.101/api/consultareservas.php';
-  static const updatereservas = 'http://192.168.0.101/api/updatereservas.php';
-  static const insertarreservas = 'http://192.168.0.101/api/insertarreservas.php';
-  static const updatedisponibilidadreservar = 'http://192.168.0.101/api/updatedisponibilidadreservar.php';
-  static const updatedisponibilidadcancelar = 'http://192.168.0.101/api/updatedisponibilidadcancelar.php';
-  static const updatevisitas = 'http://192.168.0.101/api/updatevisitas.php';
-  static const obtenerdatosprediccion = 'http://192.168.0.101/api/obtenerdatosprediccion.php';
+  static const hostConnect = 'https://desarrollomovilg3.azurewebsites.net//conexion.php';
+  static const consultalogin = 'https://desarrollomovilg3.azurewebsites.net//consultalogin.php';
+  static const consultasalones = 'https://desarrollomovilg3.azurewebsites.net//consultasalones.php';
+  static const consultadisponibilidades = 'https://desarrollomovilg3.azurewebsites.net//consultadisponibilidades.php';
+  static const updateperfil = 'https://desarrollomovilg3.azurewebsites.net//updateperfil.php';
+  static const consultareservas = 'https://desarrollomovilg3.azurewebsites.net//consultareservas.php';
+  static const updatereservas = 'https://desarrollomovilg3.azurewebsites.net//updatereservas.php';
+  static const insertarreservas = 'https://desarrollomovilg3.azurewebsites.net//insertarreservas.php';
+  static const updatedisponibilidadreservar = 'https://desarrollomovilg3.azurewebsites.net//updatedisponibilidadreservar.php';
+  static const updatedisponibilidadcancelar = 'https://desarrollomovilg3.azurewebsites.net//updatedisponibilidadcancelar.php';
+  static const updatevisitas = 'https://desarrollomovilg3.azurewebsites.net//updatevisitas.php';
+  static const obtenerdatosprediccion = 'https://desarrollomovilg3.azurewebsites.net//obtenerdatosprediccion.php';
 }
